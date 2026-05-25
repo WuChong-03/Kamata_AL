61f87546256a744bf0adcb3b818ca9bc8190c3f9
wch0126whj <wch0126whj@gmail.com>
2026-05-25 15:59:47 +0900

    更新笔记到04_03_04_GUI, 修改一些旧笔记问题

--- AL3/000_课题用引擎文件夹/DirectXGame/main.cpp
+++ AL3/000_课题用引擎文件夹/DirectXGame/main.cpp
@@ -5,65 +5,141 @@ using namespace KamataEngine;
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
-	//初期化区
+	// 初始化区
 	Initialize(L"GC2C_02_ゴ_チュウ");
-
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-	// 图片句柄
-	uint32_t spriteTextureHandle = 0;
-	spriteTextureHandle = TextureManager::Load("image.png");
+	#pragma region 01_图片初始化
+		// 图片句柄
+		uint32_t spriteTextureHandle = 0;
+		spriteTextureHandle = TextureManager::Load("test/image.png");
 
-	// 模型贴图句柄
-	uint32_t modelTextureHandle = 0;
-	modelTextureHandle = TextureManager::Load("uvChecker.png");
+		// 图片实例
+		Sprite* sprite = nullptr;
+		sprite = Sprite::Create(spriteTextureHandle, {100, 50});
+	#pragma endregion
 
-	//图片实例
-	Sprite* sprite = nullptr;
-	sprite = Sprite::Create(spriteTextureHandle, {100, 50});
+	#pragma region 02_模型初始化
+		// 模型贴图句柄
+		uint32_t modelTextureHandle = 0;
+		modelTextureHandle = TextureManager::Load("test/image.png");
 
-	//模型实例
-	Model* model = nullptr;
-	model = Model::Create();
+		// 模型实例
+		Model* model = nullptr;
+		model = Model::Create();
 
-	// WorldTransform 与 Camera 的实例创建, 初始化
-	WorldTransform worldTransform;
-	worldTransform.Initialize();
-	Camera camera;
-	camera.Initialize();
+		// 操作模型用的 WorldTransform 与 Camera 的实例创建, 初始化
+		WorldTransform worldTransform;
+		worldTransform.Initialize();
+		Camera camera;
+		camera.Initialize();
+	#pragma endregion
 
-	//主循环
+	#pragma region 03_音频初始化
+		// 音频句柄
+		uint32_t soundDataHandle = 0;
+		soundDataHandle = Audio::GetInstance()->LoadWave("test/music.wav");
+
+		// 播放音源(循环)并 保存播放中的音频句柄
+		uint32_t voiceHandle = 0;
+		voiceHandle = Audio::GetInstance()->PlayWave(soundDataHandle, true);
+		#pragma endregion
+
+	#pragma region 04_ImGui初始化
+	#ifdef _DEBUG
+		// 取得实例对象
+		ImGuiManager* imguiManager = ImGuiManager::GetInstance();
+
+		// 输入框与滑动条演示用数组
+	    float GuiTestArray[3] = {0, 0, 0};
+	#endif
+	#pragma endregion
+
+	// 主循环
 	while (true) {
-		if (Update()) {
-			break;
+		if (Update()) break;
+
+	#pragma region 04_ImGui更新处理
+	#ifdef _DEBUG
+		imguiManager->Begin();
+
+		ImGui::ShowDemoWindow();
+
+		// Debug 文本
+		ImGui::Text("TestText %d", 2050);
+
+		#pragma region 输入框与滑动条
+
+		// 输入框初始化
+		ImGui::InputFloat(
+		    "array[0][0]",		// 输入框名字
+		    &GuiTestArray[0]);		// 控制的变量
+
+		ImGui::SliderFloat(
+		    "array[0][0]", // 滑动条名字
+		    &GuiTestArray[0],    // 控制的变量
+		    0.0f,           // 可滑动的最小值
+		    1.0f);          // 可滑动的最大值
+
+		ImGui::InputFloat3(
+		    "array", // 输入框名字
+		    GuiTestArray);  // 控制的变量
+
+		// 滑动条初始化
+		ImGui::SliderFloat3(
+		    "array", // 滑动条名字
+		    GuiTestArray,    // 控制的变量
+		    0.0f,           // 可滑动的最小值
+		    1.0f);          // 可滑动的最大值
+	#pragma endregion
+
+		imguiManager->End();
+	#endif
+	#pragma endregion
+
+	#pragma region 03_音频控制与键盘输入
+		// 按下空格键瞬间停止音频
+		if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+			Audio::GetInstance()->StopWave(voiceHandle);
 		}
+	#pragma endregion
 
-		//图片移动
-		Vector2 position1 = sprite->GetPosition();
-		position1.x += 2.0f;
-		sprite->SetPosition(position1);
+	#pragma region 01_图片更新处理
+			//图片移动
+			Vector2 position1 = sprite->GetPosition();
+			position1.x += 2.0f;
+			sprite->SetPosition(position1);
+	#pragma endregion
 
-		dxCommon->PreDraw();
+	#pragma region 绘制
+	dxCommon->PreDraw();
 
-		//图片绘制
-		Sprite::PreDraw();
-		sprite->Draw();
-		Sprite::PostDraw();
+	// 01_Sprite
+	//Sprite::PreDraw();
+	//sprite->Draw();
+	//Sprite::PostDraw();
 
-		// Model 绘制
-		Model::PreDraw();
-		model->Draw(worldTransform, camera, modelTextureHandle);
-		Model::PostDraw();
+	// 02_Model
+	Model::PreDraw();
+	model->Draw(worldTransform, camera, modelTextureHandle);
+	Model::PostDraw();
 
-		dxCommon->PostDraw();
+	// 04_Imgui
+	#ifdef _DEBUG
+	imguiManager->Draw();
+	#endif
+	dxCommon->PostDraw();
+	#pragma endregion
+
 	}
 
-	//释放区
-	delete sprite;
-	sprite = nullptr;
+	#pragma region 释放区
+		delete sprite;
+		sprite = nullptr;
 
-	delete model;
-	model = nullptr;
+		delete model;
+		model = nullptr;
+	#pragma endregion
 
 	Finalize();
 	return 0;
