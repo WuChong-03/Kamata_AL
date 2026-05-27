8af190734e5c3b73f41f15eda682999196fc753a
wch0126whj <wch0126whj@gmail.com>
2026-05-27 14:39:19 +0900

    同步项目文件夹为04_03

--- AL3/000_课题用引擎文件夹/DirectXGame/GameScene.h
+++ AL3/000_课题用引擎文件夹/DirectXGame/GameScene.h
@@ -1,10 +1,40 @@
+// GameScene.h
 #pragma once
 #include "KamataEngine.h"
 
-class GameScene
-{
+class GameScene {
 public:
 	void Initialize();
 	void Update();
 	void Draw();
+	~GameScene();
+
+private:
+#pragma region 01_图片
+	uint32_t spriteTextureHandle_ = 0;
+	KamataEngine::Sprite* sprite_ = nullptr;
+#pragma endregion
+
+#pragma region 02_模型
+	uint32_t modelTextureHandle_ = 0;
+	KamataEngine::Model* model_ = nullptr;
+	KamataEngine::WorldTransform worldTransform_;
+#pragma endregion
+
+#pragma region 03_音频
+	uint32_t soundDataHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+#pragma endregion
+
+#pragma region 04_ImGui
+#ifdef _DEBUG
+	KamataEngine::ImGuiManager* imguiManager_ = nullptr;
+	float GuiTestArray_[3] = {0, 0, 0};
+#endif
+#pragma endregion
+
+#pragma region 06_Debug摄像机
+	KamataEngine::DebugCamera* debugCamera_ = nullptr;
+#pragma endregion
+
 };
--- AL3/000_课题用引擎文件夹/DirectXGame/main.cpp
+++ AL3/000_课题用引擎文件夹/DirectXGame/main.cpp
@@ -1,69 +1,41 @@
-#include <Windows.h>
+#include "GameScene.h"
 #include "KamataEngine.h"
+#include <Windows.h>
 
 using namespace KamataEngine;
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
-	//初期化区
+	// 初始化区
 	Initialize(L"GC2C_02_ゴ_チュウ");
-
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-	// 图片句柄
-	uint32_t spriteTextureHandle = 0;
-	spriteTextureHandle = TextureManager::Load("image.png");
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
 
-	// 模型贴图句柄
-	uint32_t modelTextureHandle = 0;
-	modelTextureHandle = TextureManager::Load("uvChecker.png");
-
-	//图片实例
-	Sprite* sprite = nullptr;
-	sprite = Sprite::Create(spriteTextureHandle, {100, 50});
-
-	//模型实例
-	Model* model = nullptr;
-	model = Model::Create();
-
-	// WorldTransform 与 Camera 的实例创建, 初始化
-	WorldTransform worldTransform;
-	worldTransform.Initialize();
-	Camera camera;
-	camera.Initialize();
-
-	//主循环
+	// 主循环
 	while (true) {
-		if (Update()) {
+
+	//更新处理
+		if (Update())
 			break;
-		}
 
-		//图片移动
-		Vector2 position1 = sprite->GetPosition();
-		position1.x += 2.0f;
-		sprite->SetPosition(position1);
+		gameScene->Update();
 
+	//绘制处理
 		dxCommon->PreDraw();
 
-		//图片绘制
-		Sprite::PreDraw();
-		sprite->Draw();
-		Sprite::PostDraw();
+		gameScene->Draw();
 
-		// Model 绘制
-		Model::PreDraw();
-		model->Draw(worldTransform, camera, modelTextureHandle);
-		Model::PostDraw();
+		// 07_AxisIndicator坐标指示器绘制
+		AxisIndicator::GetInstance()->Draw();
 
 		dxCommon->PostDraw();
 	}
 
 	//释放区
-	delete sprite;
-	sprite = nullptr;
-
-	delete model;
-	model = nullptr;
+	delete gameScene;
+	gameScene = nullptr;
 
 	Finalize();
 	return 0;
